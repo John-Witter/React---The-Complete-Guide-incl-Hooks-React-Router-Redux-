f89bcd1d8518124eee950e355a2dcab6f78e055b
John-Witter <mrsolginski@gmail.com>
2021-10-16 20:07:27 -0400

    initial commit

---
+++ Section 2: JavaScript Refresher/11. Understanding let and const.js
@@ -0,0 +1,21 @@
+/*
+    let, const, & var are used to create variables in JS
+
+    encouraged to not use var
+
+    use let for variable values
+
+    use const if you plan on creating a constant value
+        -most common
+
+    
+*/
+/* EXAMPLE */
+
+// var myName = "Max"; // var and let allow
+// let myName = "Max"; // myName to change on line 20
+const myName = "Max"; // const does not
+console.log(myName)
+
+myName = 'Manu'
+console.log(myName)
