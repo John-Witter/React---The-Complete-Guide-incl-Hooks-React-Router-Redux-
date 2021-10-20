74f1103e8ee893ba71f4d1de008e48210f994f73
John-Witter <mrsolginski@gmail.com>
2021-10-20 12:38:16 -0400

    ExpenseItem.css w/classNames in ExpenseItem.js

--- section-3-react-basics-&-working-with-components/01-starting-setup/src/components/ExpenseItem.js
+++ section-3-react-basics-&-working-with-components/01-starting-setup/src/components/ExpenseItem.js
@@ -1,8 +1,17 @@
-function ExpenseItem () {
-    return (
-        <h2>Expense Item</h2>
-    )
+import './ExpenseItem.css'
+
+// display amount of expense and date
+
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>Date</div>
+      <div className="expense-item__description">
+        <h2>Title</h2>
+        <div className="expense-item__price">Amount</div>
+      </div>
+    </div>
+  );
 }
 
-
-export default ExpenseItem
+export default ExpenseItem;
