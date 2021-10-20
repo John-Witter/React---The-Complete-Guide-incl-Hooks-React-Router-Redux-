d94b02a476cb6067be96727cf24dafa2c57ba299
John-Witter <mrsolginski@gmail.com>
2021-10-20 13:37:52 -0400

    ExpenseItem changed expenseDate to day, month, year using toLocaleString() and getFullYear()

--- section-3-react-basics-&-working-with-components/01-starting-setup/src/components/ExpenseItem.js
+++ section-3-react-basics-&-working-with-components/01-starting-setup/src/components/ExpenseItem.js
@@ -7,14 +7,20 @@ function ExpenseItem(props) {
     // const expenseDate = new Date(2021, 2, 28); //2 = March, month[0] = January
     // const expenseTitle = "Car Insurance";
     // const expenseAmount = 294.67;
-    const expenseDate = props.date; 
+    const day = props.date.toLocaleString("en-US", { day: "2-digit" }); 
+    const month = props.date.toLocaleString("en-US", { month: "long" }); 
+    const year = props.date.getFullYear(); 
     const expenseTitle = props.title;
     const expenseAmount = props.amount;
 
     return (
         <div className="expense-item">
             {/* Date is an object and can't be rendered as such */}
-            <div>{expenseDate.toISOString()}</div>
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
             <div className="expense-item__description">
                 <h2>{expenseTitle}</h2>
                 <div className="expense-item__price">{expenseAmount}</div>
