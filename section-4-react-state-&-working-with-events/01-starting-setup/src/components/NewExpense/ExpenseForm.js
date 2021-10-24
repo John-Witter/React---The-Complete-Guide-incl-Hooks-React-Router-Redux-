07aef1078f40b3faa1de5d0c1bb7ecd31c965648
John-Witter <mrsolginski@gmail.com>
2021-10-24 17:45:18 -0400

    ExpenseForm now uses 1 state obj instead of 3 individual slices of state

--- section-4-react-state-&-working-with-events/01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ section-4-react-state-&-working-with-events/01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -1,25 +1,40 @@
 // users should be able to:
 // enter a title, amount, and use a date picker to enter a date
-import { useState } from 'react'
+import { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+    // const [enteredTitle, setEnteredTitle] = useState('')
+    // const [enteredAmount, setEnteredAmount] = useState('')
+    // const [enteredDate, setEnteredDate] = useState('')
 
-    const [enteredTitle, setEnteredTitle] = useState('')
-    const [enteredAmount, setEnteredAmount] = useState('')
-    const [enteredDate, setEnteredDate] = useState('')
+    // refactor to use a single state obj
+    const [userInput, setUserInput] = useState({
+        enteredTitle: "",
+        enteredAmount: "",
+        enteredDate: "",
+    });
 
     const titleChangeHandler = (e) => {
-        setEnteredTitle(e.target.value)
-    }
+        setUserInput({
+            ...userInput, // spread oprtr pull key:value pairs from userInput
+            enteredTitle: e.target.value,
+        });
+    };
 
     const amountChangeHandler = (e) => {
-        setEnteredAmount(e.target.value)
-    }
+        setUserInput({
+            ...userInput, 
+            enteredAmount: e.target.value,
+        });
+    };
 
     const dateChangeHandler = (e) => {
-        setEnteredDate(e.target.value);
-    }
+        setUserInput({
+            ...userInput, 
+            enteredDate: e.target.value,
+        });
+    };
 
     return (
         <form>
