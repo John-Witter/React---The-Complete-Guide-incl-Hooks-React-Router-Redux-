f4bb04394381fe0be35d54fe84e8cbd6c74f2864
John-Witter <mrsolginski@gmail.com>
2021-10-26 10:34:14 -0400

    ChartBar dynamically renders fill-height

--- section-5-rendering-lists-&-conditional-content/01-starting-setup/src/components/Chart/ChartBar.js
+++ section-5-rendering-lists-&-conditional-content/01-starting-setup/src/components/Chart/ChartBar.js
@@ -1,5 +1,29 @@
 import "./CharBar.css";
 
-const ChartBar = () => {};
+// receives props from Chart
+// bar__fill height depends on the data/value received for maxValue
+const ChartBar = (props) => {
+    // calculate chartbar fill height based on maxValue
+    let barFillHeight = "0%"; // will be css
+
+    if (props.maxValue > 0) {
+        // get % b/t 0-100 of bar fill height, round to nearest int
+        // convert to string w/ + '%';
+        // this value will be a css value
+        barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
+    }
+
+    return (
+        <div className="chart-bar">
+            <div className="char-bar__inner">
+                <div
+                    className="chart-bar__fill"
+                    style={{ height: barFillHeight }}
+                ></div>
+            </div>
+            <div className="chart-bar__label">{props.label}</div>
+        </div>
+    );
+};
 
 export default ChartBar;
