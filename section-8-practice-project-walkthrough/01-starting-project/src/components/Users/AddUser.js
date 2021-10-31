ff5e62c448c4206c793df0200df4437e323fa5b9
John-Witter <mrsolginski@gmail.com>
2021-10-30 21:25:28 -0400

    AddUser now has controlled inputs

--- section-8-practice-project-walkthrough/01-starting-project/src/components/Users/AddUser.js
+++ section-8-practice-project-walkthrough/01-starting-project/src/components/Users/AddUser.js
@@ -1,21 +1,39 @@
 // Render something on the screen that allows the user to enter Name & Age
 // and a button to submit
+import { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 
 const AddUser = () => {
+    const [enteredUsername, setEnteredUsername] = useState("");
+    const [enteredAge, setEnteredAge] = useState("");
+
     const addUserHandler = (event) => {
         event.preventDefault();
+        console.log("enteredUsername:", enteredUsername);
+        console.log("enteredage:", enteredAge);
+    };
+
+    const usernameChangeHandler = (event) => {
+        setEnteredUsername(event.target.value);
+    };
+
+    const ageChangeHandler = (event) => {
+        setEnteredAge(event.target.value);
     };
 
     return (
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
-                <input id="username" type="text" />
+                <input
+                    id="username"
+                    type="text"
+                    onChange={usernameChangeHandler}
+                />
                 <label htmlFor="age">Age</label>
-                <input id="age" type="text" />
+                <input id="age" type="text" onChange={ageChangeHandler} />
                 <Button type="submit">Add User</Button>
             </form>
         </Card>
