f7b7ba28872c83ee8bc8e4e3402cab6d2e24b1c8
John-Witter <mrsolginski@gmail.com>
2022-01-07 16:31:22 -0500

    Header & HeaderCartButton in section-11

--- section-19-advanced-redux/01-starting-project/src/App.js
+++ section-19-advanced-redux/01-starting-project/src/App.js
@@ -1,17 +1,86 @@
-import { useSelector } from "react-redux";
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import { Fragment, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { uiActions } from "./store/ui-slice";
+import Notification from "./components/UI/Notification";
 
+// out here to avoid changing when the component re-renders
+let isInitial = true; // used to avoid running useEffect on 1st render
 
 function App() {
-  const showCart = useSelector(state => state.ui.cartIsVisible);
-  return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
-  );
+    const showCart = useSelector((state) => state.ui.cartIsVisible);
+
+    // use notification to conditionally render the Notification component
+    // and to send extra data to the Notification component
+    const notification = useSelector((state) => state.ui.notification);
+    const cart = useSelector((state) => state.cart);
+    const dispatch = useDispatch();
+
+    // save cart data to firebase
+    // each time the cart data changes
+    useEffect(() => {
+        const sendCartData = async () => {
+            dispatch(
+                uiActions.showNotification({
+                    status: "pending",
+                    title: "sending",
+                    message: "Sending cart data!",
+                })
+            );
+            const res = await fetch(
+                "https://section-19-advanced-redux-default-rtdb.firebaseio.com/cart.json",
+                {
+                    method: "PUT",
+                    body: JSON.stringify(cart),
+                }
+            );
+
+            if (!res.ok) {
+                throw new Error("Sending cart data failed.");
+            }
+
+            // if success, show success notification
+            dispatch(
+                uiActions.showNotification({
+                    status: "success",
+                    title: "Success!",
+                    message: "Sent cart data successfully!",
+                })
+            );
+        };
+
+        if (isInitial) {
+            isInitial = false;
+            return; // don't run on the first render, don't send cart data
+        }
+            sendCartData().catch((error) => {
+                dispatch(
+                    uiActions.showNotification({
+                        status: "error",
+                        title: "Error!",
+                        message: "Sending cart data failed!",
+                    })
+                );
+            });
+    }, [cart, dispatch]);
+
+    return (
+        <Fragment>
+            {notification && (
+                <Notification
+                    status={notification.status}
+                    title={notification.title}
+                    message={notification.message}
+                />
+            )}
+            <Layout>
+                {showCart && <Cart />}
+                <Products />
+            </Layout>
+        </Fragment>
+    );
 }
 
 export default App;
--- section-19-advanced-redux/01-starting-project/src/store/ui-slice.js
+++ section-19-advanced-redux/01-starting-project/src/store/ui-slice.js
@@ -8,19 +8,30 @@ import { createSlice } from "@reduxjs/toolkit";
 // initialState
 // reducers is a map of each case for actions you want to handle w/the reducer
 const uiSlice = createSlice({
-    name: 'ui',
-    initialState: { cartIsVisible: false }, // is cart visible?
+    name: "ui",
+    initialState: { cartIsVisible: false, notification: null },
     reducers: {
         toggle(state) {
-            // reduxToolkit allows us to write mutating code 
+            // reduxToolkit allows us to write mutating code
             // it uses Immer which actually makes the state immutable by creating a new state
             state.cartIsVisible = !state.cartIsVisible;
-        }
-    }
-}); 
+        },
+        // dispatch showNotification when we start sending data, 
+        // when we're done sending the data, and if we have an error
+        showNotification(state, action) {
+            // notification should be in action.payload
+            // status could be: pending, error, success
+            state.notification = {
+                status: action.payload.status,
+                title: action.payload.title,
+                message: action.payload.message,
+            };
+        },
+    },
+});
 
 // ui acions
 
-export const uiActions = uiSlice.actions; 
+export const uiActions = uiSlice.actions;
 
 export default uiSlice;
